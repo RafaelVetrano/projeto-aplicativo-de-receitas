4d9f538f259a346bc8a2d11b172e42b0baec531e
Rodrigo Frangel <soumuitorodrigo@gmail.com>
2022-07-15 17:32:05 -0300

    Implementa testes do Header

---
+++ src/tests/Header.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+// import Foods from '../components/Foods';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+describe('O componente Header', () => {
+  it('deve mostrar um elemento <h1> com o nome da página correspondente', () => {
+    renderWithRouter(<App />);
+    // const title = screen.getByRole('heading', { name: /foods/i, level: 1 })
+    const title = screen.getByTestId('page-title');
+    expect(title).toBeInTheDocument();
+  })
+
+  it('deve ter dois botões: Profile e Search', () => {
+    renderWithRouter(<App />);
+    const buttons = screen.getAllByRole('button');
+    const profile = screen.getByTestId('profile-top-btn');
+    const search = screen.getByTestId('search-top-btn');
+
+    expect(profile).toBeInTheDocument();
+    expect(search).toBeInTheDocument();
+    expect(buttons).toHaveLength(2);
+  })
+
+  it('deve redirecionar à página de perfil ao clicar no botão Profile', () => {
+    // const { history } = renderWithRouter(<App />);
+
+    // userEvent.click(screen.getByTestId('profile-top-btn'));
+    // expect(history.location.pathname).toBe('/profile')
+  })
+
+  it('deve exibir a barra de pesquisa ao clicar no botão Search', () => {
+    renderWithRouter(<App />);
+    const searchBtn = screen.getByTestId('search-top-btn');
+    userEvent.click(searchBtn);
+    
+    const searchBar = screen.getByTestId('search-input');
+    expect(searchBar).toBeInTheDocument();
+    userEvent.click(searchBtn);
+    expect(searchBar).not.toBeInTheDocument();
+  })
+})
