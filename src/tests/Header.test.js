4ff0eb14206074f1bebfc35da52061e4c15feb8f
Rodrigo Frangel <soumuitorodrigo@gmail.com>
2022-07-22 19:22:30 -0300

    Conserta os testes e req 27 (+ css)

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,37 +1,31 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
-import Foods from '../components/Foods';
+import App from '../App';
 import userEvent from '@testing-library/user-event';
 
 describe('O componente Header', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+    userEvent.type(screen.getByTestId('email-input'), 'exemplo@hotmail.com');
+    userEvent.type(screen.getByTestId('password-input'), '1234567');
+    userEvent.click(screen.getByTestId('login-submit-btn'));
+  });
+
   it('deve mostrar um elemento <h1> com o nome da página correspondente', () => {
-    renderWithRouter(<Foods />);
-    // const title = screen.getByRole('heading', { name: /foods/i, level: 1 })
     const title = screen.getByTestId('page-title');
     expect(title).toBeInTheDocument();
   })
 
   it('deve ter dois botões: Profile e Search', () => {
-    renderWithRouter(<Foods />);
-    const buttons = screen.getAllByRole('button');
     const profile = screen.getByTestId('profile-top-btn');
     const search = screen.getByTestId('search-top-btn');
 
     expect(profile).toBeInTheDocument();
     expect(search).toBeInTheDocument();
-    expect(buttons).toHaveLength(2);
-  })
-
-  it('deve redirecionar à página de perfil ao clicar no botão Profile', () => {
-    const { history } = renderWithRouter(<Foods />);
-
-    userEvent.click(screen.getByTestId('profile-top-btn'));
-    expect(history.location.pathname).toBe('/profile')
   })
 
   it('deve exibir a barra de pesquisa ao clicar no botão Search', () => {
-    renderWithRouter(<Foods />);
     const searchBtn = screen.getByTestId('search-top-btn');
     userEvent.click(searchBtn);
     
