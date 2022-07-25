ee6d74c2ee4e5ac4f520802a9b8b9bda4167bab9
Podence <podence23@gmail.com>
2022-07-25 15:30:27 -0300

    resolve testes da pagina Profile

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -3,19 +3,28 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
+import Profile from '../pages/Profile';
+
 
 describe('A página Profile', () => {
-  beforeEach(() => {
-    renderWithRouter(<App />);
+  it('deve constar o email do usuario na tela', () => {
+    const { history } = renderWithRouter(<App />);
 
     userEvent.type(screen.getByTestId('email-input'), 'exemplo@hotmail.com');
     userEvent.type(screen.getByTestId('password-input'), '1234567');
     userEvent.click(screen.getByTestId('login-submit-btn'));
     userEvent.click(screen.getByTestId('profile-top-btn'));
-
-  });
+    const emailEl = screen.getByTestId('profile-email')
+    expect(emailEl).toBeInTheDocument();
+  })
 
   it('deve ter três botões: Done Recipes, Favorite Recipes e Logout', () => {
+    const { history } = renderWithRouter(<App />);
+
+    userEvent.type(screen.getByTestId('email-input'), 'exemplo@hotmail.com');
+    userEvent.type(screen.getByTestId('password-input'), '1234567');
+    userEvent.click(screen.getByTestId('login-submit-btn'));
+    userEvent.click(screen.getByTestId('profile-top-btn'));
     const doneRecipes = screen.getByTestId('profile-done-btn');
     const favoriteRecipes = screen.getByTestId('profile-favorite-btn');
     const logout = screen.getByTestId('profile-logout-btn');
@@ -26,17 +35,46 @@ describe('A página Profile', () => {
   })
 
   it('deve ser redirecionada à página Done Recipes ao clicar no botão respectivo', () => {
+    const { history } = renderWithRouter(<App />);
+
+    userEvent.type(screen.getByTestId('email-input'), 'exemplo@hotmail.com');
+    userEvent.type(screen.getByTestId('password-input'), '1234567');
+    userEvent.click(screen.getByTestId('login-submit-btn'));
+    userEvent.click(screen.getByTestId('profile-top-btn'));
+
     userEvent.click(screen.getByTestId('profile-done-btn'));
     expect(history.location.pathname).toBe('/done-recipes');
   })
 
   it('deve ser redirecionada à página Favorite Recipes ao clicar no botão respectivo', () => {
+    const { history } = renderWithRouter(<App />);
+
+    userEvent.type(screen.getByTestId('email-input'), 'exemplo@hotmail.com');
+    userEvent.type(screen.getByTestId('password-input'), '1234567');
+    userEvent.click(screen.getByTestId('login-submit-btn'));
+    userEvent.click(screen.getByTestId('profile-top-btn'));
     userEvent.click(screen.getByTestId('profile-favorite-btn'));
     expect(history.location.pathname).toBe('/favorite-recipes');
   })
 
   it('deve ser redirecionada à página principal ao clicar em Logout', () => {
+    const { history } = renderWithRouter(<App />);
+
+    userEvent.type(screen.getByTestId('email-input'), 'exemplo@hotmail.com');
+    userEvent.type(screen.getByTestId('password-input'), '1234567');
+    userEvent.click(screen.getByTestId('login-submit-btn'));
+    userEvent.click(screen.getByTestId('profile-top-btn'));
     userEvent.click(screen.getByTestId('profile-logout-btn'));
     expect(history.location.pathname).toBe('/');
   })
+})
+
+describe('Testa profile renderizando direto', () => {
+  it('deve constar o email do usuario na tela', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/profile')
+    const emailEl = screen.getByTestId('profile-email')
+    expect(emailEl).toBeInTheDocument();
+  })
+
 })
